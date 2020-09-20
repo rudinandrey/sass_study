f4a683ad71bf0cd1085ace80798cd32d641d1a14
rudinandrey <rudinandrey@yandex.ru>
2020-09-20 15:55:21 +0300

    next step 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 const {src, dest, watch} = require("gulp");
 const sass = require("gulp-sass");
+var browserSync = require('browser-sync').create();
 
 let prepros = 'scss';
 
 function sassTask() {
-    return src('scss/')
+    return src('scss/*.scss')
         .pipe(sass())
-        .pipe(dest('css/'));
+        .pipe(dest('css/'))
+        .pipe(browserSync.stream());
+}
+
+function sync() {
+    return browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+}
+
+function serveTask() {
+    browserSync.init({
+        server: "./"
+    });
+    watch("scss/*.scss", sassTask);
+    watch("*.html").on("change", browserSync.reload);
 }
 
 exports.sass = sassTask;
+exports.browsersync = sync;
+exports.serve = serveTask;
